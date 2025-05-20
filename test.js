91c785336b0a22b4e7d01ddbf8f14334deedda4d
JeppeOEM <jappa669@gmail.com>
2025-05-20 15:32:14 +0200

    links

---
+++ test.js
@@ -0,0 +1,20 @@
+const links = `<pre id="ansi-art">
+    ...<span style="color:#0A0"> <span style="color:#AAA">. .           ..:            ∙\`:.....<span style="color:#0A0"> <span style="color:#AAA">....<span style="color:#0A0"> <span style="color:#AAA">. .           ..:            ∙\`:
+:            .. . :.  . ::         :: :    :            .. . :.  . ::         :
+:.. ..:      ∙                 .....:.:.. .:.. ..:      ∙                 .....
+             :      ...::. . ..:                        :      ...::. . ..:
+             ::... .:..        .                        ::... .:..        .
+
+<b>═══════════════════════════════════════════════════════════════════════════════</b></span></span></span></span></span></span>
+<b><span style="color:#000">:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::</span></b>
+<b><span style="color:#000">:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::</span></b>
+<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:<b><span style="color:#000">░</span></b>:
+<b><span style="color:#000">:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::</span></b>
+<b><span style="color:#000">:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::</span></b>
+<b>═══════════════════════════════════════════════════════════════════════════════</b>
+.<span style="color:#0A0"> <span style="color:#AAA">. .           ..:            ∙\`:\`∙.<span style="color:#0A0"> <span style="color:#AAA">.<span style="color:#0A0"> <span style="color:#AAA">. ..<span style="color:#0A0"> <span style="color:#AAA">. .           ..:            ∙\`:\`∙
+           .. . :.  . ::         :..                  .. . :.  . ::         :..
+. ..:      ∙                 ......:. . ..:. ..:      ∙                 ......:
+           :      ...::. . ..:                        :      ...::. . ..:
+           ::... .:..        .                        ::... .:..        .</span></span></span></span></span></span></span></span>
+</pre>`
